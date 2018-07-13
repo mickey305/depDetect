60f8e1b6cad3d24d8f3d26cfa432ea9be3ab1cbd
ysugimura <ysugimura@spiderman>
2018-07-13 12:48:26 +0900

    修正

--- src/com/cm55/depDetect/impl/Import.java
+++ src/com/cm55/depDetect/impl/Import.java
@@ -1,81 +1,103 @@
 package com.cm55.depDetect.impl;
 
-public class Import  {
+/**
+ * 一つのimport文を保持するオブジェクト
+ * @author ysugimura
+ */
+class Import  {
 
   /**
    * フルのimportパス
    * com.cm55.*, com.cm55.SampleClass.*等
    */
-  public final String fullPath;
+  final String fullPath;
   
-  /** static参照 */
-  public final boolean statical;
-  
-  /** 
-   * このimportが参照するパッケージ。
-   * import自体がクラスを参照していても、ここではパッケージを取得する。
-   */
-  PkgNodeImpl pkgNode;
+  /** static参照。import static文の場合 */
+  final boolean statical;
   
   /** import名称とstaticフラグを指定する */
-  public Import(String fullPath, boolean statical) {
+  Import(String fullPath, boolean statical) {
     this.fullPath = fullPath;
     this.statical = statical;
   }
   
-  @Override
-  public String toString() {
-    return fullPath + "," + statical;
-  }
-  
-  /** ルートノードを指定し、このimportのノードを取得する */
-  public void setNode(PkgNodeImpl root) {
-
-    // nullが返されることは無い
-    NodeImpl foundNode = root.findNode(fullPath);
-    
-    // クラスノードが見つかった場合。クラスノードの親のパッケージノードを設定する
-    if (foundNode instanceof ClsNodeImpl) {
-      pkgNode = foundNode.parent;
-      return;
-    }
-    
-    // パッケージノードの場合。見つかったノードのパスを差し引き、残りが".*"の場合にのみOK
-    String foundPath = foundNode.toString();
-    String restPath = fullPath.substring(foundPath.length());
-    if (restPath.equals(".*")) {
-      pkgNode = (PkgNodeImpl)foundNode;
-      return;      
-    }
-  }
-  
   /**
-   * このimport文の依存パッケージを求める
+   * このimport文が指し示すパッケージを取得する。
+   * importが仮にクラスを示していたとしても、そのクラスの所属するパッケージが取得される。
+   * <p>
+   * 取得されるパッケージは元々解析対象ソースとして指定されたもののみになる。
+   * 一般にimport文は、字面だけをみても、パッケージを参照するのかクラスを参照するのかはわからない。例えば、以下の様な場合
+   * </p>
+   * <pre>
+   * import com.cm55.depDetect.Const.*;
+   * </pre>
+   * <p>
+   * Constがクラスであれば、クラス内のすべての内部クラスを指定することになるし、Constがパッケージであれば、パッケージ内のすべてのクラスとなる。
+   * このように字面からは判断ができず、実際のパッケージツリーがあってはじめて判別可能になる。
+   * </p>
+   * 
    * @param thisPkg このimport文が存在するクラスのパッケージ
-   * @return 依存パッケージ、もしくはnull（不明）
+   * @return このimport文が指しします依存パッケージ、もしくはnull（不明）
    */
-  public PkgNodeImpl getDependency(PkgNodeImpl thisPkg) {
+  ImportDependency getDependency(PkgNodeImpl thisPkg) {
 
     // ルートノードを取得する
     PkgNodeImpl root = thisPkg.getRoot();
     
-    // nullが返されることは無い
+    // ルートから、import文のフルパスを探す。nullが返されることは無い
     NodeImpl foundNode = root.findNode(fullPath);
     
     // クラスノードが見つかった場合。クラスノードの親のパッケージノードを設定する
     if (foundNode instanceof ClsNodeImpl) {
-      if (foundNode.parent == thisPkg) return null;
-      return foundNode.parent;
+      // 自分のパッケージへの依存は無視すること
+      if (foundNode.parent == thisPkg) return new ImportDependency();
+      return new ImportDependency(foundNode.parent);
     }
     
-    // パッケージノードの場合。見つかったノードのパスを差し引き、残りが".*"の場合にのみOK
+    // パッケージノードが見つかった場合。
+    // 見つかったノードのパスを差し引き、残りが".*"の場合にのみ完全なパッケージが見つかったことになる。
     if (foundNode == thisPkg) return null;
     String foundPath = foundNode.toString();
     String restPath = fullPath.substring(foundPath.length());
     if (restPath.equals(".*")) {
-      return (PkgNodeImpl)foundNode;
+      return new ImportDependency((PkgNodeImpl)foundNode);
     }
     
-    return null;
+    // 上記以外の場合は見つからない。    
+    return new ImportDependency(fullPath);
+  }
+
+  /** import文から取得した依存 */
+  public static class ImportDependency {
+    /** 自己参照フラグ */
+    final boolean selfRef;
+    
+    /** 見つかった依存パッケージノード */
+    final PkgNodeImpl found;
+    
+    /** 見つからなかった依存パッケージノード */
+    final String notFound;
+    
+    ImportDependency() {
+      this.selfRef = true;
+      found = null;
+      notFound = null;
+    }
+    ImportDependency(PkgNodeImpl found) {
+      selfRef = false;
+      this.found = found;
+      notFound = null;
+    }
+    ImportDependency(String notFound) {
+      selfRef = false;
+      found = null;
+      this.notFound = notFound;
+    }
+  }
+  
+  /** デバッグ用文字列化 */
+  @Override
+  public String toString() {
+    return fullPath + "," + statical;
   }
 }
--- src/com/cm55/depDetect/impl/Imports.java
+++ src/com/cm55/depDetect/impl/Imports.java
@@ -4,56 +4,41 @@ import java.util.*;
 import java.util.stream.*;
 
 import com.cm55.depDetect.*;
+import com.cm55.depDetect.impl.Import.*;
 
 /**
  * クラスのimport一覧
  * @author ysugimura
  */
-public class Imports {
+class Imports {
 
   /** 全importの配列 */
   public final Import[]imports;
 
   /** import無し */
-  public Imports() {
+  Imports() {
     imports = new Import[0];
   }
 
   /** {@link Import}配列を与える */
-  public Imports(Import[]imports) {
+  Imports(Import[]imports) {
     this.imports = imports;
   }
 
   /** 全{@link Import}をストリームとして取得する */
-  public Stream<Import>stream() {
+  Stream<Import>stream() {
     return Arrays.stream(imports);
   }
   
-  /** 各{@link Import}に{@link NodeImpl}を設定する　*/
-  public Set<String> setNode(PkgNodeImpl root) {
-    Set<String>noNodeSet = new HashSet<String>();
-    stream().forEach(imp-> {
-      imp.setNode(root);
-      if (imp.pkgNode == null) {
-        noNodeSet.add(imp.toString());
-      }
-    });
-    return noNodeSet;
-  }
-  
-  /** このimport一覧に指定パッケージノードが含まれるか */
-  public boolean contains(PkgNodeImpl node) {
-    return stream().map(i->i.pkgNode == node).findAny().orElse(false);
-  }
-  
-  
-  public Deps createDependencies(PkgNodeImpl thisPkg) {
+  /** このimport文配列による依存パッケージ集合を作成する */
+  Deps createDependencies(PkgNodeImpl thisPkg) {
     Deps.Builder builder = new Deps.Builder();
     stream().forEach(imp-> {
-      PkgNodeImpl dependency = imp.getDependency(thisPkg);
-      if (dependency != null) builder.add(dependency);
+      ImportDependency impDep = imp.getDependency(thisPkg);
+      if (!impDep.selfRef && impDep.found != null) {
+        builder.add(impDep.found);
+      }
     });
     return builder.build();
-  }
-  
+  }  
 }
